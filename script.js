c205bdf9510a913dbe8116fe770969674ae439a3
Bryanna Vinge <bevinge711@gmail.com>
2021-09-20 11:04:53 -0500

    reorganization on lunch break- commenting on js what needs to be figured out

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 var timeElement = document.querySelector(".timer");
 var startButton = document.querySelector("#startButton");
-var answerbtn = document.querySelector("#submitanswer");
+var answerA =document.querySelector (".a");
+var answerB =document.querySelector (".b");
+var answerC = document.querySelector (".c");
+
+answerC= false;
+answerB= false;
+answerA= true;
+
+
+
 console.log(timeElement)
 console.log(startButton)
 
 
+startButton.addEventListener("click",function(event) {
+event.preventDefault;
 
-startButton.addEventListener("click",function() {
+//I need this button to also hide this content and show the next content
 
 var secondsLeft = 61;
 
@@ -18,6 +29,7 @@ function countDown() {
         if(secondsLeft === 0) {
             clearInterval(timeInterval);
         }
+
     }, 1000);
 } 
 
@@ -25,95 +37,28 @@ countDown();
 
 })
 
-var answerbtn = document.querySelector("#submitanswer")
-answerbtn.addEventListener("click", changeQuestion);
+answerA.addEventListener("click", function(event){
+    event.preventDefault;
+    // function to hide content and show content
+    // answerA will never affect the countdown
+    });
     
-function changeQuestion(){
-
-    var question = document.getElementById ("question");
-    var answer1 = document.getElementById("skin1");
-    var answer2 = document.getElementById ("muscle1");
-    var answer3 = document.getElementById("skeleton1");
-
-    if(question.innerText === "What is HTML known as when comparing it to the human body?") {
-        question.innerText ==="What is CSS know as when comparing it to the human body?";
-        
-    }
-    else if(question.innerHTML ==="What is CSS know as when comparing it to the human body?") {
-        question.innerHTML === "The special character denoting an id in CSS is:";
-    }
-
-    else if(question.innerHTML === "The special character denoting an id in CSS is:") {
-        question.innerHTML === "GitLab, GitHub, and GitBash are all owned by the same company:";
-    }
+    answerB.addEventListener("click", function(event){
+     // function to hide content and show content
+     // function to subtract 10 from the timer
+     event.preventDefault;   
+        clearInterval(timeInterval);
+        secondsLeft = (secondsLeft - 10);
+        timeElement.textContent = secondsLeft + " seconds left!";
+    });
     
-    else if (question.innerHTML === "GitLab, GitHub, and GitBash are all owned by the same company:"){
-    question.innerHTML === "An Array uses which special characters? ";
-    }
-
-    else if (question.innerHTML === "An Array uses which special characters? ") {
-        question.innerHTML === "What occupies the innermost box in the box model?";
-    }
-
-    if (answer1.innerHTML==="Skin") {
-        answer1.innerHTML ==="Skeleton";
-    }
-
-    else if (innerHTML === "Skeleton") {
-        answer1.innerHTML === "%";
-    }
-
-    else if ( innerHTML === "%") {
-        answer1.innerHTML === "I don't know";
-    }
-
-    else if (innerHTML === "I don't know") {
-        answer1.innerHTML === " ( ) ";
-    }
-
-    else if(innerHTML === " ( ) ") {
-        answer1.innerHTML === "Border"
-    }
-
     
-    if (answer2.innerHTML === "Muscle") {
-        answer2.innerHTML === "Muscle";
-    }
+    answerC.addEventListener("click", function(event){
+        event.preventDefault;
+    // function to hide content and show content
+    // function to subtract 10 from the timer
+        clearInterval(timeInterval);
+        secondsLeft = (secondsLeft - 10);
+        timeElement.textContent = secondsLeft + " seconds left!";
+       });
 
-    else if(answer2.innerHTML === "Muscle") {
-        answer2.innerHTML === " . ";
-
-    }
-    
-    else if (answer2.innerHTML === " . ") {
-        answer2.innerHTML === "True";
-    }
-
-    else if(answer2.innerHTML === "True") {
-        answer2.innerHTML === " { } ";
-    }
-
-    else if(answer2.innerHTML === " { } ") {
-        amswer2.innerHTML === "Padding";
-    }
-
-    if(answer3.innerHTML === "Skeleton") {
-        answer3.innerHTML === "Skin";
-    }
-
-    else if(answer3.innerHTML === "Skin") {
-        answer3.innerHTML ===" # ";
-    }
-
-    else if( answer3.innerHTML ===" # ") {
-        answer3.innerHTML === "False";
-    }
-
-    else if( answer3.innerHTML === "False") {
-        answer3.innerHTML === " [ ] "
-    }
-
-    else if( answer3.innerHTML ===" [ ] ") {
-        answer3.innerHTML === "Content";
-    }
-}

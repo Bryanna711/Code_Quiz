cdaaec8da7926c91bffe83fc6465c49a12ae0900
Bryanna Vinge <bevinge711@gmail.com>
2021-09-22 16:10:32 -0500

    post tutor session

--- script.js
+++ script.js
@@ -4,77 +4,77 @@ var highScoreBtn = document.querySelector("#highScoreBtn");
 var returnBtn = document.querySelector("#returnBtn")
 var resetBtn = document.querySelector("#resetTimer");
 
-var answerA1 =document.querySelector ("#question1 .a");
-var answerB1 =document.querySelector ("#question1 .b");
-var answerC1 = document.querySelector ("#question1 .c");
-var answerA2 =document.querySelector ("#question2 .a");
-var answerB2 =document.querySelector ("#question2 .b");
-var answerC2 =document.querySelector ("#question2 .c");
-var answerA3 =document.querySelector ("#question3 .a");
-var answerB3 =document.querySelector ("#question3 .b");
-var answerC3 = document.querySelector ("#question3 .c");
-var answerA4 =document.querySelector ("#question4 .a");
-var answerB4 =document.querySelector ("#question4 .b");
-var answerC4 = document.querySelector ("#question4 .c");
-var answerA5 =document.querySelector ("#question5 .a");
-var answerB5 =document.querySelector ("#question5 .b");
-var answerC5 = document.querySelector ("#question5 .c");
-var answerA6 =document.querySelector ("#question6 .a");
-var answerB6 =document.querySelector ("#question6 .b");
-var answerC6 = document.querySelector ("#question6 .c");
+var answerA1 = document.querySelector("#question1 .a");
+var answerB1 = document.querySelector("#question1 .b");
+var answerC1 = document.querySelector("#question1 .c");
+var answerA2 = document.querySelector("#question2 .a");
+var answerB2 = document.querySelector("#question2 .b");
+var answerC2 = document.querySelector("#question2 .c");
+var answerA3 = document.querySelector("#question3 .a");
+var answerB3 = document.querySelector("#question3 .b");
+var answerC3 = document.querySelector("#question3 .c");
+var answerA4 = document.querySelector("#question4 .a");
+var answerB4 = document.querySelector("#question4 .b");
+var answerC4 = document.querySelector("#question4 .c");
+var answerA5 = document.querySelector("#question5 .a");
+var answerB5 = document.querySelector("#question5 .b");
+var answerC5 = document.querySelector("#question5 .c");
+var answerA6 = document.querySelector("#question6 .a");
+var answerB6 = document.querySelector("#question6 .b");
+var answerC6 = document.querySelector("#question6 .c");
 
 var submitBtn = document.querySelector("#submitBtn");
 
-var scoresList= document.querySelector("#scoresList");
+var scoresList = document.querySelector("#scoresList");
 var scoreForm = document.querySelector("#userNameForm");
 var userNameInput = document.querySelector("#userName");
 
-var hScores= [];
-var userName= [];
+var secondsLeft = 60;
+var timeInterval;
+var hScores = [];
 
 
 console.log(timeElement)
+console.log(timeInterval)
 console.log(startButton)
 
 
-startButton.addEventListener("click",function() {
-    function startQuiz(){
-        document.getElementById("landing").style.display="none";
-        document.getElementById("question1").style.display="block";
-     }
-     startQuiz();
-     countDown();
+startButton.addEventListener("click", function () {
+    function startQuiz() {
+        document.getElementById("landing").style.display = "none";
+        document.getElementById("question1").style.display = "block";
+    }
+    startQuiz();
+    timeInterval=setInterval(countDown,1000)
 });
-    
 
-    var secondsLeft = 59;
-    var timeInterval = -1;
-    function countDown() {
-            timeInterval = setInterval(function(){
-            secondsLeft--;
-            timeElement.textContent = secondsLeft + " seconds left!";
 
-            if(secondsLeft === 0) {
-                clearInterval(timeInterval);
-            }
-            
-        }, 1000);
+function countDown() {
+    // timeInterval = setInterval(function(){
+    secondsLeft--;
+    timeElement.textContent = secondsLeft + " seconds left!";
+
+    if (secondsLeft <= 0) {
+        stopTimer();
     }
 
-    function stopTimer(){
-        clearInterval(timeInterval);
-        timeInterval = -1;
+};
+
+
+function stopTimer() {
+    clearInterval(timeInterval);
+}
+
+resetBtn.addEventListener("click", function () {
+    if (secondsLeft < 60) {
+        secondsLeft = secondsLeft + (60 - secondsLeft);
+        countDown();
     }
 
-resetBtn.addEventListener("click", function(){
-    if(secondsLeft< 60){
-    secondsLeft = secondsLeft+(60-secondsLeft);
-    countDown();}
-  
 })
-    
 
-highScoreBtn.addEventListener("click", function(){
+
+highScoreBtn.addEventListener("click", function () {
     document.getElementById("landing").style.display = "none";
     document.getElementById("question1").style.display = "none";
     document.getElementById("question2").style.display = "none";
@@ -85,241 +85,245 @@ highScoreBtn.addEventListener("click", function(){
     document.getElementById("submit").style.display = "none";
     document.getElementById("highScore").style.display = "block";
     stopTimer();
-   
+
 })
 
-returnBtn.addEventListener("click", function(){
+returnBtn.addEventListener("click", function () {
     document.getElementById("landing").style.display = "block";
     document.getElementById("highScore").style.display = "none";
-   
+    secondsLeft=60;
+
 })
 
-answerA1.addEventListener("click", function(){
-    
-    function changeQuestion(){
-        document.getElementById("question1").style.display = "none";
-        document.getElementById("question2").style.display ="block";
-        }
-        changeQuestion();
-    });
-    
-answerB1.addEventListener("click", function(){
+answerA1.addEventListener("click", function () {
 
-    function changeQuestion(){
+    function changeQuestion() {
         document.getElementById("question1").style.display = "none";
-        document.getElementById("question2").style.display ="block";
-        }
-        changeQuestion();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+        document.getElementById("question2").style.display = "block";
+    }
+    changeQuestion();
+});
+
+answerB1.addEventListener("click", function () {
+
+    function changeQuestion() {
+        document.getElementById("question1").style.display = "none";
+        document.getElementById("question2").style.display = "block";
+    }
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
     }
     wrongAnswer();
-     });
-       
-answerC1.addEventListener("click", function(){
+});
 
-    function changeQuestion(){
+answerC1.addEventListener("click", function () {
+
+    function changeQuestion() {
         document.getElementById("question1").style.display = "none";
-        document.getElementById("question2").style.display ="block";
-        }
-        changeQuestion();    
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+        document.getElementById("question2").style.display = "block";
+    }
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
     }
     wrongAnswer();
-    });
-    
-answerA2.addEventListener("click", function(){
-    
-        function changeQuestion(){
-            document.getElementById("question2").style.display = "none";
-            document.getElementById("question3").style.display ="block";
-            }
-            changeQuestion();
-        });
-         
-answerB2.addEventListener("click", function(){
+});
 
-    function changeQuestion(){
+answerA2.addEventListener("click", function () {
+
+    function changeQuestion() {
         document.getElementById("question2").style.display = "none";
-        document.getElementById("question3").style.display ="block";
-        }
-        changeQuestion();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+        document.getElementById("question3").style.display = "block";
     }
-    wrongAnswer();  
+    changeQuestion();
+});
 
-    });
-    
-    
-answerC2.addEventListener("click", function(){
+answerB2.addEventListener("click", function () {
 
-    function changeQuestion(){
+    function changeQuestion() {
         document.getElementById("question2").style.display = "none";
-        document.getElementById("question3").style.display ="block";
-        }
-        changeQuestion();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+        document.getElementById("question3").style.display = "block";
+    }
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
     }
     wrongAnswer();
 
-        });
+});
 
-answerA3.addEventListener("click", function(){
 
-    function changeQuestion(){
-        document.getElementById("question3").style.display = "none";
-        document.getElementById("question4").style.display ="block";
-        }
-        changeQuestion();
-    });    
-    
-answerB3.addEventListener("click", function(){
+answerC2.addEventListener("click", function () {
 
-    function changeQuestion(){
-        document.getElementById("question3").style.display = "none";
-        document.getElementById("question4").style.display ="block";
-        }
-        changeQuestion();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+    function changeQuestion() {
+        document.getElementById("question2").style.display = "none";
+        document.getElementById("question3").style.display = "block";
     }
-    wrongAnswer(); 
-
-    });
-        
-answerC3.addEventListener("click", function(){
-
-    function changeQuestion(){
-        document.getElementById("question3").style.display = "none";
-        document.getElementById("question4").style.display ="block";
-        }
-        changeQuestion();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
-    }
-    wrongAnswer();
-        });
-
-answerA4.addEventListener("click", function(){
-
-    function changeQuestion(){
-        document.getElementById("question4").style.display = "none";
-        document.getElementById("question5").style.display ="block";
-        }
-        changeQuestion();
-    });
-    
-answerB4.addEventListener("click", function(){
-
-    function changeQuestion(){
-        document.getElementById("question4").style.display = "none";
-        document.getElementById("question5").style.display ="block";
-        }
-        changeQuestion();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
-    }
-    wrongAnswer();  
-
-    });   
-    
-answerC4.addEventListener("click", function(){
-
-    function changeQuestion(){
-        document.getElementById("question4").style.display = "none";
-        document.getElementById("question5").style.display ="block";
-        }
-        changeQuestion();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
     }
     wrongAnswer();
 
-        });
+});
 
-answerA5.addEventListener("click", function(){
+answerA3.addEventListener("click", function () {
 
-        function changeQuestion(){
-            document.getElementById("question5").style.display = "none";
-            document.getElementById("question6").style.display ="block";
-            }
-            changeQuestion();
-        });             
-        
-answerB5.addEventListener("click", function(){
+    function changeQuestion() {
+        document.getElementById("question3").style.display = "none";
+        document.getElementById("question4").style.display = "block";
+    }
+    changeQuestion();
+});
 
-    function changeQuestion(){
+answerB3.addEventListener("click", function () {
+
+    function changeQuestion() {
+        document.getElementById("question3").style.display = "none";
+        document.getElementById("question4").style.display = "block";
+    }
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
+    }
+    wrongAnswer();
+
+});
+
+answerC3.addEventListener("click", function () {
+
+    function changeQuestion() {
+        document.getElementById("question3").style.display = "none";
+        document.getElementById("question4").style.display = "block";
+    }
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
+    }
+    wrongAnswer();
+});
+
+answerA4.addEventListener("click", function () {
+
+    function changeQuestion() {
+        document.getElementById("question4").style.display = "none";
+        document.getElementById("question5").style.display = "block";
+    }
+    changeQuestion();
+});
+
+answerB4.addEventListener("click", function () {
+
+    function changeQuestion() {
+        document.getElementById("question4").style.display = "none";
+        document.getElementById("question5").style.display = "block";
+    }
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
+    }
+    wrongAnswer();
+
+});
+
+answerC4.addEventListener("click", function () {
+
+    function changeQuestion() {
+        document.getElementById("question4").style.display = "none";
+        document.getElementById("question5").style.display = "block";
+    }
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
+    }
+    wrongAnswer();
+
+});
+
+answerA5.addEventListener("click", function () {
+
+    function changeQuestion() {
         document.getElementById("question5").style.display = "none";
-        document.getElementById("question6").style.display ="block";
-        }
-        changeQuestion();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+        document.getElementById("question6").style.display = "block";
     }
-    wrongAnswer();
-    });
-            
-answerC5.addEventListener("click", function(){
+    changeQuestion();
+});
 
-    function changeQuestion(){
+answerB5.addEventListener("click", function () {
+
+    function changeQuestion() {
         document.getElementById("question5").style.display = "none";
-        document.getElementById("question6").style.display ="block";
-        }
-        changeQuestion();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+        document.getElementById("question6").style.display = "block";
+    }
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
+    }
+    wrongAnswer();
+});
+
+answerC5.addEventListener("click", function () {
+
+    function changeQuestion() {
+        document.getElementById("question5").style.display = "none";
+        document.getElementById("question6").style.display = "block";
+    }
+    changeQuestion();
+    function wrongAnswer() {
+        secondsLeft -= 10;
     }
     wrongAnswer();
 
-    });
+});
 
-answerA6.addEventListener("click", function(){
+answerA6.addEventListener("click", function () {
 
-    function endQuiz(){
+    function endQuiz() {
         document.getElementById("question6").style.display = "none";
-        document.getElementById("submit").style.display ="block";
-        }
-        endQuiz();
-        stopTimer();
-    });             
+        document.getElementById("submit").style.display = "block";
+        clearTimeout(timeInterval)
+    }
+    endQuiz();
     
-answerB6.addEventListener("click", function(){
+});
 
-    function endQuiz(){
+answerB6.addEventListener("click", function () {
+
+    function endQuiz() {
         document.getElementById("question6").style.display = "none";
-        document.getElementById("submit").style.display ="block";
-        }
-        endQuiz();
-
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+        document.getElementById("submit").style.display = "block";
+        clearTimeout(timeInterval)
     }
-    wrongAnswer(); 
-    stopTimer();
+    endQuiz();
 
-    });
-            
-answerC6.addEventListener("click", function(){
-
-    function endQuiz(){
-        document.getElementById("question6").style.display = "none";
-        document.getElementById("submit").style.display ="block";
-        }
-        endQuiz();
-    function wrongAnswer(){
-        secondsLeft = secondsLeft - 10;
+    function wrongAnswer() {
+        secondsLeft -= 10;
     }
     wrongAnswer();
-    stopTimer();
-        });
+    
 
-submitBtn.addEventListener("click", function(){
-    function submitScore(){
-    document.getElementById("submit").style.display ="none";
-    document.getElementById("highScore").style.display ="block";
+});
+
+answerC6.addEventListener("click", function () {
+
+    function endQuiz() {
+        document.getElementById("question6").style.display = "none";
+        document.getElementById("submit").style.display = "block";
+        clearTimeout(timeInterval)
+    }
+    endQuiz();
+    function wrongAnswer() {
+        secondsLeft -= 10;
+    }
+    wrongAnswer();
+    
+});
+
+submitBtn.addEventListener("click", function () {
+    function submitScore() {
+        document.getElementById("submit").style.display = "none";
+        document.getElementById("highScore").style.display = "block";
     }
 
     submitScore();
@@ -329,83 +333,33 @@ submitBtn.addEventListener("click", function(){
         return;
     }
     hScores.push(hScoreText);
-    userNameInput.value ="";
-    
+    userNameInput.value = "";
+
     storeScores();
     renderScores();
 });
 
-function renderScores(){
-    scoresList.innerHTML= "";
-    for(var i=0; i<hScores.length; i++){
-    var hScore = hScores[i];
-    var li = document.createElement("li");
-    li.textContent = hScore;
-    li.setAttribute("data-index", i);
-    scoresList.appendChild(li);
+function renderScores() {
+    scoresList.innerHTML = "";
+    for (var i = 0; i < hScores.length; i++) {
+        var hScore = hScores[i];
+        var li = document.createElement("li");
+        li.textContent = hScore;
+        li.setAttribute("data-index", i);
+        scoresList.appendChild(li);
     }
 
     var storedScores = JSON.parse(localStorage.getItem("hScores"));
     var storedScores = (localStorage.getItem("hScores"));
 
-    if(storedScores !== null){
+    if (storedScores !== null) {
         hScores = storedScores;
     }
     renderScores();
 }
 
-function storeScores(){
+function storeScores() {
     localStorage.setItem("hScores", JSON.stringify(hScores));
-    localStorage.setItem("hScores",(hScores));
+    localStorage.setItem("hScores", (hScores));
 };
 
-/*var scoresList= document.querySelector("#scoresList");
-var scoreForm = document.querySelector("#userNameForm");
-var userNameInput = document.querySelector("#userName");
-
-var hScores= [];
-var userName= [];
-        
-    var hScoreText ="";
-
-    if (hScoreText === "") {
-        return;
-    }
-
-    hScoreText = secondsLeft;
-    hScores.push(hScoreText);
-    
-    userName.push(userNameInput)
-
-    storeScores();
-    renderScores();
-});
-
-function renderScores(){
-
-    scoresList.innerHTML= "";
-
-    for(var i=0; i<hScores.length; i++){
-    var hScores = hScores[i];
-    var userNameInput = userNameInput[i]; {
-        scoresListText += userNameInput[i] + "," + hScores[i] + "\n";
-    }
-
-    var li = document.createElement("li");
-    li.textContent = scoresListText;
-    li.setAttribute("data-index", i);
-
-    scoresList.appendChild(li);
-    }
-
-    var storedScores = JSON.parse(localStorage.getItem("hScoresList"));
-
-    if(storedScores !== null){
-        scoresList = storedScores;
-    }
-    renderScores();
-}
-
-function storeScores(){
-    localStorage.setItem("hScoresList",JSON.stringify(scoresList));
-};*/
